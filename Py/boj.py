8f5cb93440200a9c1bc32c34faae1d7f26f9be27
kayoung yoon <rkdkdudjd@gmail.com>
2021-09-05 22:28:34 +0900

    solved: boj 16928
    뱀과 사다리 게임: 최단거리(DFS)

--- Py/boj.py
+++ Py/boj.py
@@ -1,28 +1,38 @@
-N, M = map(int, input().split())
-arr = []
-for _ in range(N):
-    arr.append(list(map(int, input().split())))
+from collections import deque
 
+N, M = map(int, input().split())  # 사다리 개수, 뱀 개수
 
-# 그래. 판을 뒤집자 하..
+ans = [0] * 100  # 각 점으로 가는 최소 경로
+ans[0] = 0
+
+op = [i for i in range(100)]
+
+for _ in range(N + M):
+    x, y = map(int, input().split())
+    op[x - 1] = y - 1
+
 
 def solve():
-    ans = 0
-    li = [[4, 1], [2, 2], [2, 3], [2, 3], [3, 2]]
-    for i in range(5):
-        for j in range(M - li[i][0] + 1):
-            for k in range(N - li[i][1] + 1):
-                if i == 0:
-                    ans = max(ans, arr[j][k] + arr[j][k + 1], arr[j][k + 2], arr[j][k + 3])
-                elif i == 1:
-                    ans = max(ans, arr[j][k] + arr[j][k + 1] + arr[j + 1][k] + arr[j + 1][k + 1])
-                elif i == 2:
-                    ans = max(ans, arr[j][k] + arr[j + 1][k] + arr[j + 2][k] + arr[j + 1][k + 2])
-                elif i == 3:
-                    ans = max(ans, arr[j][k] + arr[j][k + 1], arr[j + 1][k + 1], arr[j + 2][k + 1])
-                else:
-                    ans = max(ans, arr[j][k] + arr[j][k + 1] + arr[j][k + 2] + arr[j + 1][k + 1])
-    return ans
+    que = deque([0])
+    while que:
+        n = que.popleft()
+        if n == 99:
+            break
+        curr = op[n]
+        ans[curr] = ans[n]
+        for i in range(1, 7):
+            if curr + i > 99:
+                continue
+            if ans[curr + i] == 0:
+                que.append(curr + i)
+                ans[curr + i] = ans[curr] + 1
+
+    return ans[99]
 
 
 print(solve())
+
+# for i in range(10):
+#     for j in range(10):
+#         print(ans[i*10+j], end=' ')
+#     print()
