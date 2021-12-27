788b33206618c9b7c70b55b1630d31f6c6f4bc28
kayoung yoon <rkdkdudjd@gmail.com>
2021-12-27 17:12:31 +0900

    정수배열로 입력받아서 재귀로 이분탐색 - 시간초과

--- kotlin/src/main/kotlin/Main.kt
+++ kotlin/src/main/kotlin/Main.kt
@@ -1,22 +1,13 @@
 import java.util.*
 
-fun binarySearch(n: Int, arr: IntArray, s: Int): Int { // return if n is in arr of s elements
-    var mid: Int = s / 2
-    var left = 0
-    var right = s - 1
-    while (left <= right) {
-        if (arr[mid] == n) {
-            return 1
-        }
-        if (arr[mid] > n) {
-            right = mid
-            mid = (left + right) / 2
-        } else {
-            left = mid + 1
-            mid = (left + right) / 2
-        }
-    }
-    return 0
+fun binarySearch(arr: IntArray, target: Int, left: Int, right: Int): Int {
+    if (left > right) return 0
+
+    val mid = (left + right) / 2
+    if (arr[mid] == target) return 1
+
+    if (arr[mid] < target) return binarySearch(arr, target, left + 1, right)
+    return binarySearch(arr, target, left, right - 1)
 }
 
 // BOJ 1920. 수찾기
@@ -27,10 +18,12 @@ fun main() = with(Scanner(System.`in`)) {
         arr[i] = nextInt()
     }
     arr.sort()
-    repeat(nextInt()) {
-        print("${binarySearch(nextInt(), arr, N)} ")
+    val M = nextInt()
+    val targets = IntArray(M)
+    for (i in 0 until M) {
+        targets[i] = nextInt()
     }
-    bw.flush()
-    bw.close()
-    br.close()
+    targets.forEach {
+        println(binarySearch(arr, it, 0, N-1))
+    }
 }
