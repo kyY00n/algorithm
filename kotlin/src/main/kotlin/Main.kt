9051fe8760cddc6541c93cc755df4e38545a91c0
kayoung yoon <rkdkdudjd@gmail.com>
2021-12-27 17:20:06 +0900

    이분탐색 메서드를 클래스 run으로 해도 시간초과가 뜬다

--- kotlin/src/main/kotlin/Main.kt
+++ kotlin/src/main/kotlin/Main.kt
@@ -1,29 +1,35 @@
 import java.util.*
+import kotlin.collections.ArrayList
 
-fun binarySearch(arr: IntArray, target: Int, left: Int, right: Int): Int {
-    if (left > right) return 0
+class BOJ1920 {
+    fun binarySearch(arr: List<Int>, target: Int, left: Int, right: Int): Int {
+        if (left > right) return 0
 
-    val mid = (left + right) / 2
-    if (arr[mid] == target) return 1
+        val mid = (left + right) / 2
+        if (arr[mid] == target) return 1
 
-    if (arr[mid] < target) return binarySearch(arr, target, left + 1, right)
-    return binarySearch(arr, target, left, right - 1)
+        if (arr[mid] < target) return binarySearch(arr, target, left + 1, right)
+        return binarySearch(arr, target, left, right - 1)
+    }
 }
+
 
 // BOJ 1920. 수찾기
 fun main() = with(Scanner(System.`in`)) {
     val N = nextInt()
-    val arr = IntArray(N)
-    for (i in 0 until N) {
-        arr[i] = nextInt()
+    val arr: MutableList<Int> = ArrayList()
+    repeat(N) {
+        arr.add(nextInt())
     }
     arr.sort()
     val M = nextInt()
-    val targets = IntArray(M)
-    for (i in 0 until M) {
-        targets[i] = nextInt()
+    val targets: MutableList<Int> = ArrayList()
+    repeat(M) {
+        targets.add(nextInt())
     }
-    targets.forEach {
-        println(binarySearch(arr, it, 0, N-1))
+    BOJ1920().run {
+        targets.forEach {
+            println(binarySearch(arr, it, 0, N-1))
+        }
     }
 }
