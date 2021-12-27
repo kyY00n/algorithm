d286d28c41b5620d707a2919cb7727b535ed3d4b
kayoung yoon <rkdkdudjd@gmail.com>
2021-12-27 14:35:29 +0900

    solved: boj 1260.DFS와 BFS

--- kotlin/src/main/kotlin/Main.kt
+++ kotlin/src/main/kotlin/Main.kt
@@ -1,57 +1,53 @@
 import java.util.*
 
 var visited = BooleanArray(1001)
-val adjList = Array(1001) { PriorityQueue<Int>() }
+val adjMat = Array(1001) { IntArray(1001) { 0 } }
 
-fun dfs(n: Int) {
+fun dfs(n: Int, size: Int) {
     print("$n ")
     visited[n] = true
-    val adjListIter = adjList[n].iterator()
-    while (adjListIter.hasNext()) {
-        val curr = adjListIter.next()
-        if (!visited[curr]) {
-            dfs(curr)
+    for (i in 0..size) {
+        if (adjMat[n][i] == 1) {
+            if (!visited[i]) {
+                dfs(i, size)
+            }
         }
     }
 }
 
-fun bfs(n: Int) {
+fun bfs(n: Int, size: Int) {
     val queue: Queue<Int> = LinkedList()
     queue.add(n)
     visited[n] = true
-    while (queue.isNotEmpty()) {
-        val i = queue.remove()
-        print("$i ")
-        val adjListIter = adjList[i].iterator()
-        while (adjListIter.hasNext()) {
-            val curr = adjListIter.next()
-            if (!visited[curr]) {
-                queue.add(curr)
-                visited[curr] = true
+    while(queue.isNotEmpty()) {
+        val head = queue.poll()
+        print("$head ")
+        for (i in 0..size) {
+            if (adjMat[head][i] == 1) {
+                if (!visited[i]) {
+                    queue.add(i)
+                    visited[i] = true
+                }
             }
         }
     }
-
-
 }
 
 // BOJ 1260. DFS와 BFS
 fun main() = with(Scanner(System.`in`)) {
-    nextInt()
+    val N = nextInt()
     val M = nextInt()
     val V = nextInt()
 
     repeat(M) {
         val s = nextInt()
         val e = nextInt()
-        adjList[s].add(e)
-        adjList[e].add(s)
+        adjMat[s][e] = 1
+        adjMat[e][s] = 1
     }
 
-    dfs(V)
+    dfs(V, N)
     visited = BooleanArray(1001)
     println()
-    bfs(V)
+    bfs(V, N)
 }
-
-// priorityQueue 씀
