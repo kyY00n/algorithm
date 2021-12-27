d8590b3a90fdd7f2660028e6bdc5ffa6342dccb1
kayoung yoon <rkdkdudjd@gmail.com>
2021-12-27 17:02:33 +0900

    Revert "bufferedReader 사용했지만 시간초과"
    This reverts commit f6de3ee01ba09d6c9a6d756821a17f1a1aa903d4.

--- kotlin/src/main/kotlin/Main.kt
+++ kotlin/src/main/kotlin/Main.kt
@@ -1,16 +1,34 @@
-// BOJ 1920. 수 찾기
-fun main() {
-    val br = System.`in`.bufferedReader()
-    val bw = System.out.bufferedWriter()
-    val N = br.readLine().toInt()
-    var arr: List<Int> = br.readLine().split(" ").map { it.toInt() }
-    arr = arr.sorted()
-    br.readLine()
-    val input = br.readLine().split(" ").map { it.toInt() }.toIntArray()
-    for (i in input) {
-        val res: String = if (arr.binarySearch(i) >= 0) "1" else "0"
-        bw.write(res)
-        bw.newLine()
+import java.util.*
+
+fun binarySearch(n: Int, arr: IntArray, s: Int): Int { // return if n is in arr of s elements
+    var mid: Int = s / 2
+    var left = 0
+    var right = s - 1
+    while (left <= right) {
+        if (arr[mid] == n) {
+            return 1
+        }
+        if (arr[mid] > n) {
+            right = mid
+            mid = (left + right) / 2
+        } else {
+            left = mid + 1
+            mid = (left + right) / 2
+        }
+    }
+    return 0
+}
+
+// BOJ 1920. 수찾기
+fun main() = with(Scanner(System.`in`)) {
+    val N = nextInt()
+    val arr = IntArray(N)
+    for (i in 0 until N) {
+        arr[i] = nextInt()
+    }
+    arr.sort()
+    repeat(nextInt()) {
+        print("${binarySearch(nextInt(), arr, N)} ")
     }
     bw.flush()
     bw.close()
