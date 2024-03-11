8fb8bd628c2cf83beea2f216c510419d76d0e2d2
Rosie <dev.kayoung@gmail.com>
2024-03-11 23:26:54 +0900

    240311: leetcode 791

---
+++ java/edoctest/Fourth.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Fourth {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int score = 0;
+        for(int i = 0; i < 10; i ++) {
+            int newScore = Integer.parseInt(br.readLine());
+            int beforeAbs = Math.abs(score - 100);
+            int afterAbs = Math.abs(score + newScore - 100);
+            if (beforeAbs < afterAbs) {
+                break;
+            }
+            score += newScore;
+            if (score > 100) {
+                break;
+            }
+        }
+        System.out.println(score);
+    }
+}
