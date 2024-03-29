307035c2ea25d5a551b50ed7628452c5ea9d0921
Rosie <dev.kayoung@gmail.com>
2024-03-29 23:33:19 +0900

    solved: boj 11659. get prefix sum

---
+++ java/src/main/java/edoc/week2/homework/GetPrefixSum4.java
@@ -0,0 +1,38 @@
+package edoc.week2.homework;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// https://www.acmicpc.net/problem/11659
+public class GetPrefixSum4 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] s = br.readLine().split(" ");
+        int N = Integer.parseInt(s[0]);
+        int M = Integer.parseInt(s[1]);
+
+        int[] nums = new int[N];
+        String[] line = br.readLine().split(" ");
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(line[i]);
+        }
+
+        int[] prefixSum = new int[N];
+        prefixSum[0] = nums[0];
+        for (int i = 1; i < N; i++) {
+            prefixSum[i] = nums[i] + prefixSum[i - 1];
+        }
+
+        for(int i = 0; i < M; i++) {
+            String[] input = br.readLine().split(" ");
+            int start = Integer.parseInt(input[0]) - 1;
+            int end = Integer.parseInt(input[1]) - 1;
+
+            System.out.println(prefixSum[end] - prefixSum[start] + nums[start]);
+        }
+
+    }
+
+}
