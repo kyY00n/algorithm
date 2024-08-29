81cac597490ebc492db563c4a107f3c55cab3709
Kayoung Yoon <dev.kayoung@gmail.com>
2024-08-29 17:12:29 +0900

    edit: added description

--- java/src/main/java/leetcode/LeetCode_27.java
+++ java/src/main/java/leetcode/LeetCode_27.java
@@ -2,15 +2,13 @@ package leetcode;
 
 public class LeetCode_27 {
     public int removeElement(int[] nums, int val) {
-        int k = 0; // pointer for the next position to place a non-val element
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[k] = nums[i];
-                k++;
+        int index = 0; // initialize to 0 which represents the position of next non-target element
+        for(int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) { // if `nums[i]` is not equal to `val`
+                nums[index] = nums[i]; // then overwrite nums[index] with nums[i];
+                index++; // increase `index` by 1
             }
         }
-
-        return k;
+        return index; // length of the modified array
     }
 }
