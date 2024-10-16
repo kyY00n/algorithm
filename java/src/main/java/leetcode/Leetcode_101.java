14ef6eeb6ffebf903f3bdb961d1483af35c8c845
Kayoung Yoon <dev.kayoung@gmail.com>
2024-10-16 12:36:31 +0900

    chore: comment

--- java/src/main/java/leetcode/Leetcode_101.java
+++ java/src/main/java/leetcode/Leetcode_101.java
@@ -2,6 +2,7 @@ package leetcode;
 
 
 public class Leetcode_101 {
+
     class TreeNode {
 
         int val;
@@ -22,20 +23,28 @@ public class Leetcode_101 {
         }
 
     }
+
     public boolean isSymmetric(TreeNode root) {
         TreeNode left = root.left;
         TreeNode right = root.right;
 
+        // we have to check the symmetrical value, so pass each pointer of nodes
         return isSame(left, right);
     }
 
     private boolean isSame(TreeNode left, TreeNode right) {
+        // if one of the given pointers is null
         if (left == null || right == null) {
-            return left == right;
+            return left == right; // return if the pointers equal or not.
         }
+
+        // if the values of each node doesn't equal
         if (left.val != right.val) {
-            return false;
+            return false; // return false.
         }
+
+        // return the result of child nodes comparison. if there is non-symmetric values in child tree, it returns false.
         return isSame(left.right, right.left) && isSame(left.left, right.right);
     }
+
 }
--- java/src/main/java/leetcode/leetcode100/Leetcode_100.java
+++ java/src/main/java/leetcode/leetcode100/Leetcode_100.java
@@ -28,12 +28,16 @@ public class Leetcode_100 {
     }
 
     private boolean traverse(TreeNode p, TreeNode q) {
+        // if p or q is null, we have to check both of the pointers are null.
         if (p == null || q == null) {
             return p == q;
         }
+        // if both of the nodes have values, test if the values equal
         if (p.val != q.val) {
             return false;
         }
+
+        // and then check child node, if there is different value, we finally get false.
         return traverse(p.right, q.right) && traverse(p.left, q.left);
     }
 
