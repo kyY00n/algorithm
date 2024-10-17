a12c830e4f49d69fe6f84e37029ba17475d048ae
Kayoung Yoon <dev.kayoung@gmail.com>
2024-10-17 12:45:05 +0900

    chore: added comment

--- java/src/main/java/leetcode/Leetcode_110.java
+++ java/src/main/java/leetcode/Leetcode_110.java
@@ -3,23 +3,25 @@ package leetcode;
 public class Leetcode_110 {
 
     public boolean isBalanced(TreeNode root) {
-        if (root == null) {
-            return true;
+        if (root == null) { // we need to test if the root is null
+            return true; // we can see that the empty tree is balanced in the example.
         }
         int hi = height(root);
-        if (hi == -1) {
+        if (hi == -1) { // if the height is -1, it means that the tree is not balanced.
             return false;
         }
         return true;
     }
 
     private int height(TreeNode node) {
-        if (node == null) {
+        if (node == null) { // if the node is null, the height is 0.
             return 0;
         }
-        int left = height(node.left);
-        int right = height(node.right);
+        int left = height(node.left); // get the height of the left child node
+        int right = height(node.right); // get the height of the right child node
         if (Math.abs(left - right) > 1 || left == -1 || right == -1) {
+            // if the difference between the left height and right height is bigger than one, then this tree is not balanced, so return -1.
+            // If the left or right height is -1, we have to return -1 as well.
             return -1;
         }
         return Math.max(left, right) + 1;
